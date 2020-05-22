2aa4da61f9bccf48c5aa3a5fe06d2e32619ab646
leonardo1294 <33595596+leonardo1294@users.noreply.github.com>
2020-05-22 17:37:15 -0300

    updated logic, resulting in more reliability in the comparison

--- index.js
+++ index.js
@@ -6,35 +6,22 @@ async function run() {
 
     console.clear()
 
-  await faceDetectionNet.loadFromDisk('./weights')
-  await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights')
-  await faceapi.nets.faceRecognitionNet.loadFromDisk('./weights')
+    await faceDetectionNet.loadFromDisk('./weights');
+    await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights');
+    await faceapi.nets.faceRecognitionNet.loadFromDisk('./weights');
 
-  const twoFaces = await canvas.loadImage('./imgTeste.jpg');
+    const faces = await canvas.loadImage('./imgTeste.jpg');
 
-  const detectionss = await faceapi.detectAllFaces(twoFaces, faceDetectionOptions);
-  const faceImages = await faceapi.extractFaces(twoFaces, detectionss)
+    const results = await faceapi.detectAllFaces(faces, faceDetectionOptions)
+                                    .withFaceLandmarks()
+                                    .withFaceDescriptors();
 
-  let count = 0;
-  try{
-    faceImages.map(img => {
-        count++;
-        saveFile(`faceDetection${count}.jpg`, img.toBuffer('image/jpeg'))
-      });
-  }catch(e){
-      console.log(e)
-  }
-  
-  const img1 = await canvas.loadImage('./out/faceDetection1.jpg')
-  const img2 = await canvas.loadImage('./out/faceDetection2.jpg')
+    const descriptors = results.map(({descriptor}) => descriptor);
 
-  const descriptor1 = await faceapi.computeFaceDescriptor(img1, faceDetectionOptions);
-  const descriptor2 = await faceapi.computeFaceDescriptor(img2, faceDetectionOptions);
+    const distance = faceapi.euclideanDistance(descriptors[0], descriptors[1]).toFixed(3);
 
-  const distance = faceapi.euclideanDistance(descriptor1, descriptor2).toFixed(3)
+    console.log(distance);
 
-  console.log(distance)
- 
-}
+};
 
-run()
+run();
